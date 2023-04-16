70fc7e4b01d0c380fceb1e72138864039d34b2f8
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2023-04-16 13:40:51 -0500

    set correct URL

--- EcoTulum/docusaurus.config.js
+++ EcoTulum/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://YeikoSantamaria.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/EcoTulum/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
