7c8d9b916f12891ca14334155ee59798aa0c1f46
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2023-04-16 15:41:05 -0500

    set url

--- EcoTulum/docusaurus.config.js
+++ EcoTulum/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://YeikoSantamaria.github.io/',
+  url: 'https://brave-pebble-094166c10.3.azurestaticapps.net/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
