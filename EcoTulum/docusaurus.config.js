e483dfc8916bf934e08b4681860aee2c73da5e63
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2023-04-18 01:57:46 -0500

    t

--- EcoTulum/docusaurus.config.js
+++ EcoTulum/docusaurus.config.js
@@ -107,7 +107,7 @@ const config = {
             items: [
               {
                 label: 'Desarrollos',
-                to: '/docs/Desarrollos',
+                to: '/docs/Desarrollos/Tulum',
               },
             ],
           },
